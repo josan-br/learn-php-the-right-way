251aa6d344f4a73ad51cc4bcfa84cf627babd546
josan-br <josantnun@outlook.com>
2026-03-03 17:03:31 -0300

    feat: create custom autoload

--- src/public/index.php
+++ src/public/index.php
@@ -2,10 +2,17 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../app/Notification/Email.php';
-require_once __DIR__ . '/../app/PaymentGateway/Paddle/CustomerProfile.php';
-require_once __DIR__ . '/../app/PaymentGateway/Paddle/Transaction.php';
-require_once __DIR__ . '/../app/PaymentGateway/Stripe/Transaction.php';
+// require_once __DIR__ . '/../app/Notification/Email.php';
+// require_once __DIR__ . '/../app/PaymentGateway/Paddle/CustomerProfile.php';
+// require_once __DIR__ . '/../app/PaymentGateway/Paddle/Transaction.php';
+// require_once __DIR__ . '/../app/PaymentGateway/Stripe/Transaction.php';
+
+// Customer autoload
+spl_autoload_register(function ($class) {
+    $path = __DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
+
+    if (file_exists($path)) require $path;
+});
 
 use App\PaymentGateway\Paddle\Transaction;
 
