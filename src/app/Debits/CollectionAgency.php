76ea90917c05b9ba76ee5eb1ac096894baeeec47
josan-br <josantnun@outlook.com>
2026-03-05 18:02:21 -0300

    refactor: move debits classes to Debits namespace

--- src/app/Debits/CollectionAgency.php
+++ src/app/Debits/CollectionAgency.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Debits;
 
 class CollectionAgency implements DebitCollector
 {
--- src/app/Debits/DebitCollector.php
+++ src/app/Debits/DebitCollector.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Debits;
 
 interface DebitCollector
 {
