3be9fd2bb6e715f0d323449f00660a493b058997
josan-br <josantnun@outlook.com>
2026-03-12 19:59:49 -0300

    chore: create Config class to centralize app configuration

---
+++ app/Config.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+/**
+ * @property-read array{host:string, username:string, password:string, database:string, driver:string}|null $database
+ */
+final class Config
+{
+    protected array $config = [];
+
+    public function __construct(array $env)
+    {
+        $this->config = [
+            'database' => [
+                'host' => $env['DB_HOST'],
+                'username' => $env['DB_USERNAME'],
+                'password' => $env['DB_PASSWORD'],
+                'database' => $env['DB_DATABASE'],
+                'driver' => $env['DB_DRIVER'] ?? 'mysql',
+            ]
+        ];
+    }
+
+    public function __get($name)
+    {
+        return $this->config[$name] ?? null;
+    }
+}
