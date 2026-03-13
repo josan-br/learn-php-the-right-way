a3c2c9e956fbe5cc9c3dd946e91c48b5c765661d
josan-br <josantnun@outlook.com>
2026-03-12 21:11:29 -0300

    feat: sign up when access home

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,12 +4,28 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\App;
+
+use App\Models\Invoice;
+use App\Models\User;
+
 use App\View;
 
 class HomeController
 {
     public function index(): View
     {
-        return View::make('index', ['title' => 'Home']);
+        $db = App::db();
+
+        $userModel = new User();
+        $invoiceModel = new Invoice();
+
+        $invoiceId = (new \App\Actions\SignUp($db, $userModel, $invoiceModel))
+            ->handle('fulano2@doe.com', 'Jawdwada Doe', 25);
+
+        return View::make('index', [
+            'title' => 'Home',
+            'invoice' => $invoiceModel->find($invoiceId)
+        ]);
     }
 }
--- views/index.php
+++ views/index.php
@@ -1,2 +1,15 @@
 <h1><?= $title ?></h1>
 <p>Welcome to the home page!</p>
+
+<hr />
+
+<div>
+    <?php if (! empty($invoice)): ?>
+        Invoice ID: <?= htmlspecialchars($invoice['id'], ENT_QUOTES) ?>
+        <br />
+        Invoice Amount: <?= htmlspecialchars($invoice['amount'], ENT_QUOTES) ?>
+        <br />
+        User: <?= htmlspecialchars($invoice['full_name'], ENT_QUOTES) ?>
+        <br />
+    <?php endif ?>
+    <div>
